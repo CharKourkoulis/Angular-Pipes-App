65669d37e3af0e6abb6751f0d794527388020e44
unknown <b.kourkoulis@gmail.com>
2020-03-03 22:14:23 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  paymentDate: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: 'VW',
+    model: 'lupo',
+    year: 2001
+  };
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.paymentDate = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
 }
